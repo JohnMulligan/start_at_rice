b54c1d7a64a33c9623a8aa18616578b6557d6dc8
John Connor (Mulligan) <john.connor.mulligan@gmail.com>
2022-10-08 14:41:49 -0500

    restructuring for AWS deployment

--- Readme.md
+++ Readme.md
@@ -8,7 +8,7 @@ All reference materials (inluding the PowerPoint) are available on the Start@Ric
 
 Some reference materials are available in the reference_materials subfolder of this repository.
 
-You *don't* need to know how to "code" to take this course, and you *won't* "learn to code" either :)
+You *don't* need to know how to "code" to take this course, and you *won't* "learn to code" in one day!
 
 What you will be introduced to:
 
@@ -22,7 +22,7 @@ What you will be introduced to:
 
 ## Software and documentation
 
-In order to actively participate in this lesson, you have to install some 
+In order to actively participate in this lesson, you have to install some basic software
 
 ### Required software installations
 
@@ -39,23 +39,121 @@ These are all free and you can install in class or beforehand.
 * Plotly:
 	* Dash: https://dash.plotly.com/installation
 
-### Optional cloud deployment platforms we'll be using
+### Cloud Deployment
 
-You don't have to register for or use the below, but we'll be walking through these options as a part of the lesson.
+I have set up 10 AWS deployment pipelines and micro apps for you. Credentials will be handed out in class.
 
-* Heroku deployment
-	* CLI installation: https://devcenter.heroku.com/articles/git
-	* Git deployment https://devcenter.heroku.com/articles/git
-* AWS deployment
+* AWS deployment documentation, for your reference
 	* Deploy directly with ElasticBeanstalk: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-flask.html
 	* Deploy via a repository using CodeCommit: https://docs.aws.amazon.com/codepipeline/latest/userguide/tutorials-simple-codecommit.html
+
+I deploy my apps to Heroku as I find it to be more straightforward, but they are deprecating their free tier, and so I will not be using this in the class. You can see how this sort of code looks in my other repositories.
+
+* Containerized R deployment to Heroku: https://github.com/JohnMulligan/covid_dashR/
+* Simple Python deployment to Heroku: https://github.com/JohnMulligan/covid_dashPy
+
+* Heroku deployment documentation
+	* CLI installation: https://devcenter.heroku.com/articles/git
+	* Git deployment https://devcenter.heroku.com/articles/git
 	
-## Accounts
+--------------------
 
-You should, but do not need to, get accounts with the following services in order to follow this lesson.
+# The app
 
-* Very useful
-	* Github: https://github.com/ (Free!)
-* Optional
-	* Heroku: https://www.heroku.com/ (not free for much longer!)
-	* AWS: https://aws.amazon.com/ "Free"
+# Adaptation of CDC's All-Cause Excess Mortality Dashboard
+
+This repo uses CDC data on excess mortality (generated to estimate the impacts of COVID-19) as an exercise in quickly building and deploying interactive data visualizations to the web, because these are the cornerstone of exploratory data analysis.
+
+csv from: https://data.cdc.gov/api/views/xkkf-xrst/rows.csv?accessType=DOWNLOAD&bom=true&format=true%20target=
+
+## Core technologies
+
+The following frameworks and services are used in what is outlined below:
+
+* Free, curated data from the CDC
+* Python
+* Pandas
+* Plotly (Dash)
+* AWS CodeCommit --> CodeDeploy --> ElasticBeanstalk
+
+## Setting up your environment (Python)
+
+In order to run through this exercise, you will need Python 3 installed, and specific packages for it. It's best to set up a virtual environment so that you can record the specifics of your installation, in order for a web server to rebuild that later on.
+
+1. Open a terminal
+
+*navigate to this folder*, and initialize a Python virtual environment.
+
+	python3 -m venv venv
+
+This command invokes the python module "venv"
+	
+2. Initialize the virtual environment
+
+Its source files live in the folder you created with the above command (the second "venv")
+
+	source venv/bin/activate
+	
+3. Install the packages
+
+These are listed in requirements.txt
+
+	pip3 install -r requirements.txt
+
+4. You're good to go! Try running an app.
+
+	python app.py
+
+## Running the applications in this repo
+
+These applications are single-page apps rendered in React on top of Flask. Plotly is truly excellent when you want to quickly make a dataset visually interactive. The curve is a steep one as you ask for more functionality, FYI.
+
+There are two apps in this repo. Each can be run simply by invoking the file in python (from within the virtual environment), like:
+	
+	python scatter.py
+	
+	python application.py
+	
+
+### What do the apps do?
+
+1. scatter.py: this application shows
+	1. For the weeks since January 1, 2020:
+	1. The number of people who
+		1. were expected to die in Texas, week by week
+		1. actually died in excess of that number
+	1. These numbers are stacked up to show the excess mortality
+	1. For weeks where the death rate was exceptionally high (above the 95% CI upper bound as determined by a Farrington outbreak detection algorithm), an "x" has been tagged to the top of the week
+1. application.py: this application shows
+	1. for the weeks since January 1, 2020:
+	1. A multi-select geographic heatmap ("choropleth")
+		1. of the United States
+		1. showing the percentage of weeks that rose above the 95% CI upper bound predicted mortality for that state based on historical data
+	1. The number of people who
+		1. Were expected to die in any state or combination of states
+		1. As selected in the choropleth
+		1. And, if only one state or the entire US is selected, it flags the exceptionally high weeks with an "x"
+
+## Deploying to AWS
+
+I've set up 10 AWS code deployment pipelines and app servers for the class. This app can be pushed as is to AWS, with a bit of configuration.
+
+In class we'll be going through these steps in detail, but I outline them below for your reference.
+
+Download the SSH keys that I've provided for you, and put these in ~/.ssh. This will give you access to the AWS Codecommit repository I set up for you.
+
+Update your git config with the below information:
+
+	Host git-codecommit.*.amazonaws.com
+	User USERNAME
+	IdentityFile ~/.ssh/PRIVATEKEYFILENAME
+
+Add the keys to your keychain: ```ssh-add ~/.ssh/PRIVATEKEYFILENAME```
+
+Pull your repository down from AWS: ```	git clone ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/START_AT_RICE_LEARNER_ID```
+
+Look at the app, make some changes, save those, and push back up. We'll look at the code deployment live together.
+
+	git add .
+	git commit -m "my changes"
+	git push
---
+++ application.py
@@ -0,0 +1,282 @@
+import dash
+from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import date
+import numpy as np
+import pandas as pd
+import json
+import re
+from vars import *
+
+####### INTITIALIZE THE APP & LOAD THE DATA #######
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+application = app.server
+#server = app.server
+df=pd.read_csv('Excess_Mortality_Estimates.csv')
+
+####### PRELIM FORMATTING AND FILTERING #######
+
+# 1. map states to their abbreviations
+df['State_Abbv']=df['State'].map(state_abbv_map)
+df['Alarm']=df['Exceeds Threshold'].map({True:"X",False:None})
+inverse_state_abbv_map={state_abbv_map[k]:k for k in state_abbv_map}
+
+# 2. FORMAT & EXTRACT DATES
+## https://pandas.pydata.org/docs/user_guide/timeseries.html
+df['Week Ending Date'] = pd.to_datetime(df['Week Ending Date'])
+df=df.sort_values("Week Ending Date")
+daterange=list(pd.unique(df['Week Ending Date']))
+
+# 3. PRELIM filtering
+## Have to choose this because the unweighted counts only exist for all-cause mortality, not for all-cause-excluding-covid
+df=df[df['Type']=='Predicted (weighted)']
+
+#4. Categorical Selectors (to be used in radio options below)
+outcomes=pd.unique(df['Outcome'])
+
+####### APP PAGE LAYOUT #######
+
+## We use Dash Bootstrap Components below.
+## Documentation: https://dash-bootstrap-components.opensource.faculty.ai/docs/components/layout/
+
+## The controls get to be a bit much in the main layout (below), so I've broken them out.
+controls=dbc.Card(
+	[
+		html.Div(
+			dcc.DatePickerRange(
+				id='daterangepicker',
+				min_date_allowed=pd.Timestamp(min(daterange)),
+				max_date_allowed=pd.Timestamp(max(daterange)),
+				start_date=pd.Timestamp(np.datetime64('2020-01-01')),
+				end_date=pd.Timestamp(max(daterange))
+			),
+			style = {"alignItems": "center", "justifyContent": "center"}
+		),
+		html.Div(
+			[
+			dcc.RadioItems(
+				outcomes,
+				outcomes[0],
+				inline=False,
+				id='outcomes_radio',
+				inputStyle={"marginLeft": "10px","marginRight":"3px"}
+			),
+			html.Hr()
+			],
+			style = {"alignItems": "center", "justifyContent": "center"}
+		),
+		html.Div(
+			[
+				html.P("",id="para",style={"margin":"10px"})
+			]
+		)
+	]
+)
+
+## Main app layout
+app.layout = dbc.Container(
+	[
+		dbc.Row(
+			[	
+				dbc.Col(
+					html.A(
+						"An adaptation of the CDC's COVID-19 excess mortality dashboard.",
+						href="https://www.cdc.gov/nchs/nvss/vsrr/covid19/excess_deaths.htm",
+						style={"margin":"10px"}
+					)
+				),
+			],
+		),
+		dbc.Row(
+			[
+				dbc.Col(
+					html.Div(
+						[controls]
+					),
+					width=12,xs=12,sm=12,md=12,lg=6
+				),
+				dbc.Col(
+					html.Div(
+						[dcc.Graph(id="choropleth_map")]
+					),
+					width=12,xs=12,sm=12,md=12,lg=6
+				)
+			]
+		),
+		dbc.Row(
+			html.Div(
+				[
+					dcc.Graph(id="graph")
+				],
+			)
+		)
+	]
+)
+
+####### CALLBACKS (Robust Interactivity) #######
+
+# Update the scatter plot and the explanatory text when:
+## The date picker is used
+## The radio buttons are used
+## The map selections are used
+
+@app.callback(
+    Output('graph', 'figure'),
+    Output('para','children'),
+	Input('daterangepicker', 'start_date'),
+	Input('daterangepicker', 'end_date'),
+	Input('choropleth_map', 'selectedData'),
+	Input('outcomes_radio','value')
+)
+def line_graph(start_date,end_date,selectedData,outcome):
+	
+	## FILTER ON OUTCOME & DATES
+	filtered=df[df['Outcome']==outcome]
+	filtered=filtered[filtered['Week Ending Date']>=start_date]
+	filtered=filtered[filtered['Week Ending Date']<=end_date]
+	
+	## SELECTED STATES
+	### handle one or multiple states, or US
+	### build some insertion text for the app
+	if selectedData is not None:
+		selectedstates=[inverse_state_abbv_map[p['location']] for p in selectedData['points']]
+	else:
+		selectedstates=["United States"]
+	
+	if selectedstates==["United States"]:
+		selectedstates_string="the United States"
+	else:
+		# a nice list to comma-separated text converter, with "and" but without oxford comma https://stackoverflow.com/a/19839338
+		selectedstates_string=", ".join(selectedstates[:-2] + [" and ".join(selectedstates[-2:])])
+	
+	### filter on the basis of the selected states
+	df2=filtered[filtered['State'].isin(selectedstates)]
+	df2=df2[df2['Outcome']==outcome]
+	
+	### & calculate the total excess
+	#### Note: CDC's total excess column in this sheet is lower than this -- they may be accidentally counting the negatives?
+	total_excess=int(df2["Excess Estimate"].sum())
+	
+	## COUNT UP ALARMS
+	### but you can't simply add up the alarms across states
+	### as that would require re-running the Farrington algo
+	### so that has to be disabled on multi-state select
+	if len(selectedstates)>1:
+		df2=df2.groupby("Week Ending Date").sum()
+		alarmlabels=[]
+		excess_alarms_text=""
+	else:
+		alarmlabels=df2["Alarm"]
+		excessweekscount=df2["Exceeds Threshold"].sum()
+		total_weeks=len(alarmlabels)
+		excess_alarms_text="%s of %s weeks in this time period exceeded the 95 percent CI upper bound for predicted mortality." %(str(excessweekscount),str(total_weeks))
+		excess_alarms_text = excess_alarms_text
+
+	## "Week Ending Date" is the index because we've grouped by it
+	## So push it back into a proper column we can call
+	df2.reset_index(inplace=True)	
+	
+	fig=go.Figure()
+	fig.add_trace(go.Scatter(
+		x=df2["Week Ending Date"],
+		y=df2["Average Expected Count"],
+		name="Average Expected Count",
+		mode="lines",
+		stackgroup="one",
+		line_shape="spline"
+	))
+
+	fig.add_trace(go.Scatter(
+		x=df2["Week Ending Date"],
+		y=df2["Excess Estimate"],
+		name="Excess Estimate",
+		mode="lines+text",
+		stackgroup="one",
+		line_shape="spline",
+		text=alarmlabels,
+		textposition="top center"
+	))
+	
+	fig.update_layout(title="Weekly Excess Mortality (%s) in %s" %(outcome,selectedstates_string))
+	
+	## Final text formatting
+	
+	start_date_str=re.search("[0-9]+-[0-9]+-[0-9]+",str(start_date)).group(0)
+	end_date_str=re.search("[0-9]+-[0-9]+-[0-9]+",str(end_date)).group(0)
+	start_year=start_date_str[0:4]
+	end_year=end_date_str[0:4]
+	
+	excess_mortality_text="Between %s and %s, %s more people died (%s) than would have been expected in %s, based on historical averages." %(start_date_str,end_date_str,f"{total_excess:,}",outcome.lower(),selectedstates_string)
+	
+	output_text = ' '.join([excess_mortality_text,excess_alarms_text])
+	
+	return(fig,output_text)
+
+# Update the map when:
+## The radio buttons are used: selecting covid only or all causes
+## The date picker is used: selecting different date ranges
+# Both of the above behaviors change the heatmap
+## Which shows percentage of weeks in that range that rise above 95% CI upper bound
+@app.callback(
+		Output('choropleth_map', 'figure'),
+		Input('daterangepicker', 'start_date'),
+		Input('daterangepicker', 'end_date'),
+		Input('outcomes_radio','value')
+)
+def update_output(start_date,end_date,outcome):	
+	
+	## FILTER ON OUTCOME & DATES
+	filtered=df[df['Outcome']==outcome]
+	filtered=filtered[filtered['Week Ending Date']>=start_date]
+	filtered=filtered[filtered['Week Ending Date']<=end_date]
+	numberofweeks=len(pd.unique(filtered['Week Ending Date']))
+
+	## GROUPING DEPENDENT ON THE ABOVE FILTERS
+	### first the state-by-state summaries
+	grouped=filtered.groupby('State_Abbv').agg(
+		{
+			'Total Excess Estimate': 'max',
+			'Exceeds Threshold': 'sum'
+		}
+	)
+	grouped=grouped.rename(columns={'Exceeds Threshold':'Weeks Exceeding Threshold'})
+	grouped['Percent Weeks Exceeding Threshold']=100*grouped['Weeks Exceeding Threshold']/numberofweeks
+	
+	## VARIABLE-DEPENDENT GRAPH STYLING
+	### documentation: https://plotly.github.io/plotly.py-docs/generated/plotly.graph_objects.Choropleth.html
+	fig = go.Figure(data=go.Choropleth(
+		locations=grouped.index,
+		z = grouped['Percent Weeks Exceeding Threshold'],
+		locationmode = 'USA-states',
+		colorscale="Reds",
+		customdata=[grouped.index,grouped['Percent Weeks Exceeding Threshold']]
+	))
+
+	fig.update_layout(
+		title_text = 'Percent of weeks exceeding 95% CI upper bound threshold',
+		geo_scope='usa',
+		dragmode=False
+	)
+
+	fig.update_traces(
+		colorbar=dict(
+		ticktext = ["20","40","60","80"],
+		tickvals = [20,40,60,80],
+		tickmode="array"
+		),
+		# turned off z auto so that when the levels change dramatically, it shows
+		# above all, between all deaths / all deaths excluding covid
+		zauto=False,
+		zmin=0,
+		zmax=100
+	)
+	
+	fig.update_layout(clickmode='event+select')
+	
+	return fig
+
+if __name__ == '__main__':
+	application.run(debug=True, port=8080)
